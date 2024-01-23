6ea08618b85bcd2ae4be2356b7b58197538d388e
jingli18 <jingli18@bu.edu>
2024-01-23 20:36:05 +0800

    feat: add zsh local

--- setup.sh
+++ setup.sh
@@ -1,13 +1,12 @@
-cp .vimrc ~/.vimrc
-cp .tmux.conf ~/.tmux.conf
-apt-get update
-apt-get install tmux
+# cp .vimrc ~/.vimrc
+# cp .tmux.conf ~/.tmux.conf
+# apt-get update
+# apt-get install tmux
 
-#apt-get install zsh
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-cd ~/.oh-my-zsh/custom/plugins
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-git clone https://github.com/zsh-users/zsh-autosuggestions.git
+# apt-get install zsh
+# cp -r .oh-my-zsh ~/
+cp -r zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/
+cp -r zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/
 cp .zshrc ~/.zshrc
 sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
 echo "source ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
